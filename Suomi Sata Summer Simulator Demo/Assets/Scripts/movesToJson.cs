bc7882d8df8b216b5cab34dad492b797e0c4cb3e
benzen89 <salomaa.leo@gmail.com>
2017-11-18 10:21:57 +0200

    Changes to tracking json writer

--- Suomi Sata Summer Simulator Demo/Assets/Scripts/movesToJson.cs
+++ Suomi Sata Summer Simulator Demo/Assets/Scripts/movesToJson.cs
@@ -8,30 +8,49 @@ public class movesToJson : MonoBehaviour {
     MyClass myObject;
 
     bool listed;
+    public string limb;
 
     void Start () {
         myObject = new MyClass();
         myObject.positions = new List<Vector3>();
+        myObject.rotations = new List<Quaternion>();
+        myObject.times = new List<float>();
 
         listed = false;
     }
 	
-	// Update is called once per frame
 	void Update () {
 
         if (!listed)
         {
+            myObject.date = System.DateTime.Now.ToString();
+            myObject.limb = limb;
             myObject.positions = new List<Vector3>();
+            myObject.times = new List<float>();
+            
             listed = true;
         }
         myObject.positions.Add(this.transform.position);
+        myObject.rotations.Add(this.transform.rotation);
+        myObject.times.Add(Time.time);
+        //Debug.Log(transform.rotation);
 
         if (Input.GetButtonDown("Fire1"))
         {
             string json = JsonUtility.ToJson(myObject);
             Debug.Log(json);
 
-            File.WriteAllText(Application.dataPath + "/Player.json", json);
+            //File.WriteAllText(Application.dataPath + "/Player.json", json);
+
+            System.DateTime moment = System.DateTime.Now;
+            int year = moment.Year;
+            int month = moment.Month;
+            int day = moment.Day;
+            int hour = moment.Hour;
+            int minute = moment.Minute;
+            int second = moment.Second;
+
+            File.WriteAllText(Application.dataPath + "/" + year + month + day + hour + minute + second + ".json", json);
         }
     }
 }
@@ -39,5 +58,9 @@ public class movesToJson : MonoBehaviour {
 [System.Serializable]
 public class MyClass
 {
+    public string date;
+    public string limb;
     public List<Vector3> positions;
+    public List<Quaternion> rotations;
+    public List<float> times;
 }
