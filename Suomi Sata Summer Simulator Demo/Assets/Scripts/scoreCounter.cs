cc4f114d6f122e38ccaf4426e6f6a6fda22d7e44
benzen89 <salomaa.leo@gmail.com>
2017-11-18 11:56:00 +0200

    Added score counter, victory text and fonts

---
+++ Suomi Sata Summer Simulator Demo/Assets/Scripts/scoreCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreCounter : MonoBehaviour {
+
+    public int score;   //how many logs have been cut?
+    public int logsTotalSpawns;    //how many logs have been spawned in total?
+    public GameObject victoryText;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            logsTotalSpawns++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            score++;
+        }
+
+        if(logsTotalSpawns >= 21)
+        {
+            Debug.Log("You got " + score + " logs out of " + logsTotalSpawns);
+            victoryText.GetComponent<TextMesh>().text = "You got " + score.ToString() + " logs out of " + logsTotalSpawns.ToString();
+        }
+	}
+}
